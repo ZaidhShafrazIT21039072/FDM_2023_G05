b921a9b823c28815d93c5964844f893f31054364
Zaidh Shafraz <87699674+ZaidhShafrazIT21039072@users.noreply.github.com>
2023-10-16 15:35:36 +0530

    Linear Regression model for our dataset
    Done by Sathis

--- app.py
+++ app.py
@@ -6,56 +6,81 @@ import numpy as np
 app = Flask(__name__)
 
 def prediction(lst):
-  filename = 'model/predictor.pickle'
-  with open(filename,'rb') as file:
-    model = pickle.load(file)
-  pred = model.predict([lst])
-  return pred
-  
-@app.route('/',methods=['POST','GET'])
+    filename = 'model/predictor.pickle'
+    with open(filename, 'rb') as file:
+        model = pickle.load(file)
+    pred_value = model.predict([lst])
+    return pred_value
+
+@app.route('/', methods=['POST', 'GET'])
 def index():
-    pred = 0
+    # return "Hello World"
+    pred_value = 0
     if request.method == 'POST':
-       ratings = request.form['Ratings']
-       ram = request.form['RAM']
-       rom = request.form['ROM']
-       mobile_size = request.form['Mobile_Size']
-       primary_cam = request.form['Primary_Cam']
-       selfie_cam = request.form['Selfi_Cam']
-       battery_power = request.form['Battery_Power']
-       brand = request.form['brand']
-       
+        ram = request.form['ram']
+        rom = request.form['rom']
+        rating = request.form['rating']
+        batterypower = request.form['batterypower']
+        mobilesize = request.form['mobilesize']
+        primarycam = request.form['primarycam']
+        selficam = request.form['selficam']
+        brandname = request.form['brandname']
 
-       feature_list = []
+        mobilesize_list = ['3.4','3.7','5.8','6.0','6.1','6.2','6.3','6.4','6.5']
+        primarycam_list = ['12','16','36','48','64']
+        selficam_list = ['8','12','24','32','84']
+        brandname_list = ['apple','nokia','oppo','redmi','samsung','vivo']
 
-       feature_list.append(float(ratings))
-       feature_list.append(float(ram))
-       feature_list.append(float(rom))
-       feature_list.append(float(mobile_size))
-       feature_list.append(float(primary_cam))
-       feature_list.append(float(selfie_cam))
-       feature_list.append(float(battery_power))
-       
 
-       brand_list=['Apple IPhone 11','Apple iPhone 12','Nokia  216','Nokia  415','Oppo  A17','Oppo  A9','Redmi A9','Redmi K20','Samsung  Galaxy A50','Samsung Galaxy','Vivo  V21','Vivo  Y81']
+
+ 
+        
+        feature_list = []
+        
+        feature_list.append(float(rating))
+        feature_list.append(int(ram))
+        feature_list.append(int(rom))
+        
+
+
+        def traverse_list1(lst, value):
+            for item in lst:
+                if item == value:
+                    feature_list.append(float(item))
+
+        traverse_list1(mobilesize_list,mobilesize)
+        
+      
+        feature_list.append(int(batterypower))
+
+        traverse_list1(primarycam_list,primarycam)
+        traverse_list1(selficam_list,selficam)
+      
+        # for item in company_list:
+        #     if item == company:
+        #         feature_list.append(1)
+        #     else:
+        #         feature_list.append(0)
+
+        def traverse_list(lst, value):
+            for item in lst:
+                if item == value:
+                    feature_list.append(1)
+                else:
+                    feature_list.append(0)
+        
+        traverse_list(brandname_list, brandname)
+
+        
+
     
-       def traverse(lst,value):
-         for item in lst:
-          if item == value:
-           feature_list.append(1)
-          else:
-           feature_list.append(0)
+        print(feature_list)
+
+        pred_value = prediction(feature_list)
+        pred_value = np.round(pred_value[0],2)
+
+    return render_template('index.html', pred_value=pred_value)
 
 
-       traverse(brand_list,brand)
-
-    
-       pred = prediction(feature_list)
-       pred = np.round(pred[0],2)
-
-
-    return render_template("index.html", pred = pred)
-
-
-if __name__ =='__main__':
-     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
