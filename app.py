ab3579a40ffb4b298b4ed516e2061150e5455389
IT21183522 <99382686+IT21183522@users.noreply.github.com>
2023-10-17 11:49:20 +0530

    Created a model using decision tree

--- app.py
+++ app.py
@@ -2,60 +2,56 @@ from flask import Flask, render_template, request
 import pickle
 import numpy as np
 
-# setup application
 app = Flask(__name__)
 
-def prediction(lst):
-  filename = 'model/predictor.pickle'
-  with open(filename,'rb') as file:
-    model = pickle.load(file)
-  pred = model.predict([lst])
-  return pred
-  
-@app.route('/',methods=['POST','GET'])
+# Create prediction function
+def prediction(input_list):
+    # Load the model
+    filename = 'model/predictor.pickle'
+    with open(filename, 'rb') as file:
+        model = pickle.load(file)
+
+    pred_value = model.predict([input_list])
+    return pred_value
+
+
+@app.route('/', methods = ['POST', 'GET'])
 def index():
     pred = 0
     if request.method == 'POST':
-       ratings = request.form['Ratings']
-       ram = request.form['RAM']
-       rom = request.form['ROM']
-       mobile_size = request.form['Mobile_Size']
-       primary_cam = request.form['Primary_Cam']
-       selfie_cam = request.form['Selfi_Cam']
-       battery_power = request.form['Battery_Power']
-       brand = request.form['brand']
-       
+        ram = request.form['ram']
+        rom = request.form['rom']
+        company = request.form['company']
+        size = request.form['mobile_size']
+        prim_cam = request.form['primary_cam']
+        selfi_cam = request.form['selfi_cam']
+        battery = request.form['battery']
 
-       feature_list = []
+        feature_list = []
+        feature_list.append(int(ram))
+        feature_list.append(int(rom))
+        feature_list.append(float(size))
+        feature_list.append(int(prim_cam))
+        feature_list.append(int(selfi_cam))
+        feature_list.append(int(battery))
 
-       feature_list.append(float(ratings))
-       feature_list.append(float(ram))
-       feature_list.append(float(rom))
-       feature_list.append(float(mobile_size))
-       feature_list.append(float(primary_cam))
-       feature_list.append(float(selfie_cam))
-       feature_list.append(float(battery_power))
-       
+        #Get the company
+        company_list = ['apple', 'nokia', 'oppo', 'redmi', 'samsung', 'vivo']
 
-       brand_list=['Apple IPhone 11','Apple iPhone 12','Nokia  216','Nokia  415','Oppo  A17','Oppo  A9','Redmi A9','Redmi K20','Samsung  Galaxy A50','Samsung Galaxy','Vivo  V21','Vivo  Y81']
-    
-       def traverse(lst,value):
-         for item in lst:
-          if item == value:
-           feature_list.append(1)
-          else:
-           feature_list.append(0)
+        for item in company_list:
+            if item == company:
+                feature_list.append(1)
+            else:
+                feature_list.append(0)
 
+        pred = prediction(feature_list)*323.55
+        pred = np.round(pred[0])
+        print(pred)
 
-       traverse(brand_list,brand)
-
-    
-       pred = prediction(feature_list)
-       pred = np.round(pred[0],2)
-
-
+        # print(feature_list)
+        
+    # return "වැඩේ ගොඩ"
     return render_template("index.html", pred = pred)
 
-
-if __name__ =='__main__':
-     app.run(debug=True)
+if __name__ == '__main__':
+    app.run(debug=True)
